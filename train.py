761a8361a48d131ae74ff46b906bd52d93cf970a
Robert Ridley <robert.n.ridley@gmail.com>
2019-12-12 15:04:18 +0800

    add some metrics

--- train.py
+++ train.py
@@ -1,3 +1,8 @@
+from time import time
+import keras.backend as K
+import numpy as np
+from sklearn.metrics import accuracy_score
+from utils.metrics import precision, recall, f1_score, false_positive_rate
 from utils.clean_data import process_legit_phish_data
 from utils.preprocess_data import split_data, extract_labels, create_vocab, read_dataset
 from utils.general_utils import get_logger, padding_email_sequences, load_word_embedding_dict, build_embedd_table
@@ -11,6 +16,11 @@ def main():
     embedding_path = 'embeddings/glove.6B.50d.txt'
     embedding = 'glove'
     embedd_dim = 50
+    epochs = 1
+    batch_size = 16
+    baby = True
+    if baby:
+        all_data = all_data[:100]
     train, dev, test = split_data(all_data)
     x_train, y_train = extract_labels(train)
     x_dev, y_dev = extract_labels(dev)
@@ -49,16 +59,112 @@ def main():
         embed_table = [embedd_matrix]
     
     model = build_simple_themis(vocab, max_token, embedd_dim, embed_table)
-    logger.info('Training')
-    model.fit(X_train, Y_train,
-            batch_size=32,
-            epochs=20)
 
-    logger.info('Evaluation')
-    loss, acc = model.evaluate(X_test, Y_test,
-                            batch_size=32)
-    logger.info('Test loss / test accuracy = {:.4f} / {:.4f}'.format(loss, acc))
+    logger.info("Initial evaluation: ")
+    best_dev_acc = -1
+    best_dev_precision = -1
+    best_dev_recall = -1
+    best_dev_f1 = -1
+    best_dev_false_pos_rate = -1
 
+    best_test_acc = -1
+    best_test_precision = -1
+    best_test_recall = -1
+    best_test_f1 = -1
+    best_test_false_pos_rate = -1
+    
+    train_pred = model.predict(X_train, batch_size=batch_size)
+    dev_pred = model.predict(X_dev, batch_size=batch_size)
+    test_pred = model.predict(X_test, batch_size=batch_size)
+    
+    train_pred = np.round(train_pred)
+    dev_pred = np.round(dev_pred)
+    test_pred = np.round(test_pred)
+
+    dev_acc = accuracy_score(Y_dev, dev_pred)
+    dev_precision = precision(Y_dev, dev_pred)
+    dev_recall = recall(Y_dev, dev_pred)
+    dev_f1 = f1_score(Y_dev, dev_pred)
+    dev_false_pos_rate = f1_score(Y_dev, dev_pred)
+
+    test_acc = accuracy_score(Y_test, test_pred)
+    test_precision = precision(Y_test, test_pred)
+    test_recall = recall(Y_test, test_pred)
+    test_f1 = f1_score(Y_test, test_pred)
+    test_false_pos_rate = f1_score(Y_test, test_pred)
+
+    if dev_acc > best_dev_acc:
+        best_dev_acc = dev_acc
+        best_dev_precision = dev_precision
+        best_dev_recall = dev_recall
+        best_dev_f1 = dev_f1
+        best_dev_false_pos_rate = dev_false_pos_rate
+
+        best_test_acc = test_acc
+        best_test_precision = test_precision
+        best_test_recall = test_recall
+        best_test_f1 = test_f1
+        best_test_false_pos_rate = test_false_pos_rate
+
+    logger.info('[DEV]   ACC:  %.3f, PREC: %.3f, REC: %.3f, F1: %.3f, FPR: %.3f \n(Best ACC: {{%.3f}}, Best PREC: {{%.3f}}, Best REC: {{%.3f}}, Best F1: {{%.3f}}, Best FPR: {{%.3f}})' % (
+        dev_acc, dev_precision, dev_recall, dev_f1, dev_false_pos_rate, 
+        best_dev_acc, best_dev_precision, best_dev_recall, best_dev_f1, best_dev_false_pos_rate))
+    logger.info('[TEST]   ACC:  %.3f, PREC: %.3f, REC: %.3f, F1: %.3f, FPR: %.3f \n(Best ACC: {{%.3f}}, Best PREC: {{%.3f}}, Best REC: {{%.3f}}, Best F1: {{%.3f}}, Best FPR: {{%.3f}})' % (
+        test_acc, test_precision, test_recall, test_f1, test_false_pos_rate, 
+        best_test_acc, best_test_precision, best_test_recall, best_test_f1, best_test_false_pos_rate))
+
+    logger.info("Train model")
+    for ii in range(epochs):
+        logger.info('Epoch %s/%s' % (str(ii + 1), epochs))
+        start_time = time()
+        model.fit(X_train, Y_train, batch_size=batch_size, epochs=1, verbose=0, shuffle=True)
+        tt_time = time() - start_time
+        logger.info("Training one epoch in %.3f s" % tt_time)
+        train_pred = model.predict(X_train, batch_size=batch_size)
+        dev_pred = model.predict(X_dev, batch_size=batch_size)
+        test_pred = model.predict(X_test, batch_size=batch_size)
+
+        train_pred = np.round(train_pred)
+        dev_pred = np.round(dev_pred)
+        test_pred = np.round(test_pred)
+
+        dev_acc = accuracy_score(Y_dev, dev_pred)
+        dev_precision = precision(Y_dev, dev_pred)
+        dev_recall = recall(Y_dev, dev_pred)
+        dev_f1 = f1_score(Y_dev, dev_pred)
+        dev_false_pos_rate = f1_score(Y_dev, dev_pred)
+
+        test_acc = accuracy_score(Y_test, test_pred)
+        test_precision = precision(Y_test, test_pred)
+        test_recall = recall(Y_test, test_pred)
+        test_f1 = f1_score(Y_test, test_pred)
+        test_false_pos_rate = f1_score(Y_test, test_pred)
+
+        if dev_acc > best_dev_acc:
+            best_dev_acc = dev_acc
+            best_dev_precision = dev_precision
+            best_dev_recall = dev_recall
+            best_dev_f1 = dev_f1
+            best_dev_false_pos_rate = dev_false_pos_rate
+
+            best_test_acc = test_acc
+            best_test_precision = test_precision
+            best_test_recall = test_recall
+            best_test_f1 = test_f1
+            best_test_false_pos_rate = test_false_pos_rate
+        
+        logger.info('[DEV]   ACC:  %.3f, PREC: %.3f, REC: %.3f, F1: %.3f, FPR: %.3f \n(Best ACC: {{%.3f}}, Best PREC: {{%.3f}}, Best REC: {{%.3f}}, Best F1: {{%.3f}}, Best FPR: {{%.3f}})' % (
+            dev_acc, dev_precision, dev_recall, dev_f1, dev_false_pos_rate, 
+            best_dev_acc, best_dev_precision, best_dev_recall, best_dev_f1, best_dev_false_pos_rate))
+        logger.info('[TEST]   ACC:  %.3f, PREC: %.3f, REC: %.3f, F1: %.3f, FPR: %.3f \n(Best ACC: {{%.3f}}, Best PREC: {{%.3f}}, Best REC: {{%.3f}}, Best F1: {{%.3f}}, Best FPR: {{%.3f}})' % (
+            test_acc, test_precision, test_recall, test_f1, test_false_pos_rate, 
+            best_test_acc, best_test_precision, best_test_recall, best_test_f1, best_test_false_pos_rate))
+
+    logger.info('[DEV]   BEST ACC:  %.3f, BEST PREC: %.3f, BEST REC: %.3f, BEST F1: %.3f, BEST FPR: %.3f' % (
+        best_dev_acc, best_dev_precision, best_dev_recall, best_dev_f1, best_dev_false_pos_rate))
+    logger.info('[TEST]   BEST ACC:  %.3f, BEST PREC: %.3f, BEST REC: %.3f, BEST F1: %.3f, BEST FPR: %.3f' % (
+        best_test_acc, best_test_precision, best_test_recall, best_test_f1, best_test_false_pos_rate))
+    
 
 if __name__ == "__main__":
     main()
---
+++ utils/metrics.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def calc_vlaues(y_true, y_pred):
+    y_true = y_true.flatten()
+    y_pred = y_pred.flatten()
+    TP = np.sum(np.logical_and(y_true == 1, y_pred == 1))
+    TN = np.sum(np.logical_and(y_true == 0, y_pred == 0))
+    FP = np.sum(np.logical_and(y_true == 0, y_pred == 1))
+    FN = np.sum(np.logical_and(y_true == 1, y_pred == 0))
+    return TP, TN, FP, FN
+
+
+def precision(y_true, y_pred):
+    TP, TN, FP, FN = calc_vlaues(y_true, y_pred)
+    return TP/(TP + FP)
+
+
+def recall(y_true, y_pred):
+    TP, TN, FP, FN = calc_vlaues(y_true, y_pred)
+    return TP/(TP + FN)
+
+
+def f1_score(y_true, y_pred):
+    prec = precision(y_true, y_pred)
+    rec = recall(y_true, y_pred)
+    return 2*((prec*rec) / (prec + rec))
+
+
+def false_positive_rate(y_true, y_pred):
+    TP, TN, FP, FN = calc_vlaues(y_true, y_pred)
+    return FP/(FP + TN)
